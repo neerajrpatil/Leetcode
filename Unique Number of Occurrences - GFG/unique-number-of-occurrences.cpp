e5c30bba124901a0fcd74a2b9c2ab0980463c8aa
neerajrpatil <118601363+neerajrpatil@users.noreply.github.com>
2023-07-13 11:14:15 +0530

    Added solution - LeetHub

---
+++ Unique Number of Occurrences - GFG/unique-number-of-occurrences.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    bool isFrequencyUnique(int n, int a[])
+    {   
+        //hash frequeicies
+        //hash k badle vector me freq store kar k,vector k sort kar k
+        //adjacent value same hai tho return false, but 2x1e9 ka vector chahiye
+        
+        //now why is hash better than vecot?
+        //max hash size will go to 1e5, in worts case, vector will go to 
+        //2e9..how am i saying this??look at constrains
+        
+        sort(a,a+n);
+        
+        unordered_map<int,int>mp;
+        int i=0,j=0;
+        while(j<n){
+            if(a[i]==a[j])j++;
+            else {
+                if(mp[j-i]++!=0)return false;
+                i=j;
+            }
+            
+        }
+        if(mp[j-i]++!=0)return false;
+        
+        return true;
+        
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        bool ans=ob.isFrequencyUnique(n,arr);
+        if(ans)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
