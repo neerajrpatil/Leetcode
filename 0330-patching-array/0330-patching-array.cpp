05d3f0603834e3be2057e27f39131ebc4a7b5d44
neerajrpatil <118601363+neerajrpatil@users.noreply.github.com>
2024-06-16 23:19:53 +0530

    Time: 9 ms (21.31%), Space: 13.6 MB (97.87%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -6,34 +6,43 @@ public:
         ref:    https://leetcode.com/problems/patching-array/solutions/5321946/why-not-you-tried-prefix-sum-greedy-approach-o-logn/
         */
 
-        //assume i get sum 3
-        //and i want to add a element 4
+        //assume i can get all sum till 3 (i.e 1,2,3)
+        //and i want to add element 4
         //then then i can add 4 to each sum to get
         //1+4=5
         //2+4=6
         //3+4=7
         //4 itslef
 
-        //but with sum 3 if i want to add 5
-
+        
         /*
+        //but with sum 3 if i want to add 5
         then sum i can get is 
         5 itslef
         1+5=6
         2+5 =7
         3+5=8
-        
+        but i wont get sum=4
+        //so i must add 4
+        to generalise?
+        if i can get sum 3, i next number in array is 5
+        i must add 4 
         */
 
         /*
-           conclusion : nums[i]<=sum+1 to get 
+           conclusion :  
+           sum>nums[i] no porblem
+           sum==nums[i] no problem
+
+           but if nums[i]-sum>1 ..
+           ..ie sum was 3,nums[i]=5, i must add element (& increase cnt) 
         */
 
         #define ll long long
         ll ans=0,sum=0;
         for(ll i=0;i<nums.size();){
             if(sum>=n)break;
-            if(i<n&&nums[i]<=sum+1){
+            if(i<n&&nums[i]-sum<=1){
                 sum+=nums[i];
                 i++;
             }
