95518fa05eb9bdee380ec7fef0123d3d85b545fe
neerajrpatil <118601363+neerajrpatil@users.noreply.github.com>
2024-06-18 20:34:48 +0530

    Time: 46 ms (76.16%), Space: 41.9 MB (30.67%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -1,15 +1,39 @@
 class Solution {
 public:
-    int maxProfitAssignment(vector<int>& d, vector<int>& p, vector<int>& worker) {
-        int ans=0,n=d.size();
-        for(int w:worker){
-            int maxi=0;
-            for(int i=0;i<n;i++){
-                if(w>=d[i]){
-                    maxi=max(maxi,p[i]);
-                }
-            }
-            ans+=maxi;
+#define V(type) vector<type>
+#define pb(vec, val) vec.push_back(val)
+#define ppb(vec) vec.pop_back()
+#define so(vec) sort(vec.begin(), vec.end())
+#define rev(vec) reverse(vec.begin(), vec.end())
+#define f0(i, n) for(int i = 0; i < n; ++i)
+#define p(vec) do { cout << "//"; for (const auto& elem : vec) cout << " " << elem; cout << " //" << endl; } while (0)
+#define minheap(type) priority_queue<type, vector<type>, greater<type>>
+#define pii pair<int,int>
+#define fi first
+#define se second
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        V(pii) v;
+        int n=difficulty.size(),m=worker.size();
+        for(int i=0;i<n;i++){
+            v.push_back({difficulty[i],profit[i]});
+        }
+        sort(begin(v),end(v),[&](pii a, pii b){
+            return a.fi<b.fi;
+        });
+        vector<int>maxTillInd(n);
+        int val=0;
+        for(int i=0;i<n;i++){
+            val=max(val,v[i].second);
+            maxTillInd[i]=val;
+        }
+        int ans=0;
+        sort(begin(difficulty),end(difficulty));
+        for(auto ele:worker){
+            auto ub=upper_bound(begin(difficulty),end(difficulty),ele);
+            int j=distance(begin(difficulty),ub);
+            if(j==0)continue;
+            j--;
+            ans+=maxTillInd[j];
         }
         return ans;
     }
