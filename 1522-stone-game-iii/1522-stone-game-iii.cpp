a45f79f9181903ecb76f3baf2a5cecae3fa75c53
neerajrpatil <118601363+neerajrpatil@users.noreply.github.com>
2024-06-19 22:54:48 +0530

    Time: 145 ms (82.52%), Space: 128.7 MB (100%) - LeetHub

--- 1522-stone-game-iii/1522-stone-game-iii.cpp
+++ 1522-stone-game-iii/1522-stone-game-iii.cpp
@@ -11,10 +11,23 @@ public:
        }
        return dp[i]=ans;
     }
-    string stoneGameIII(vector<int>& stoneValue) {
+    string stoneGameIII(vector<int>& v) {
         
-        vector<long long>dp(stoneValue.size(),LLONG_MIN);
-        int ans=f(0,stoneValue,dp);
+        // vector<long long>dp(stoneValue.size(),LLONG_MIN);
+        // int ans=f(0,stoneValue,dp);
+        int n=v.size();
+        vector<int>dp(3,0);
+        for(int i=n-1;i>=0;i--){
+            int ans=INT_MIN;
+            int val=0;
+            for(int j=i;j<=i+2&&j<v.size();j++){
+            val+=v[j];
+            ans=max(ans,val-dp[(j+1)%3]);
+            }
+            dp[i%3]=ans;
+        }
+        int ans=dp[0];
+        
         if(ans>0)return "Alice";
         else if(ans<0)return "Bob";
         else return "Tie";
