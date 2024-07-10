26dd0afc5203351554460bc1f5ddafc0e081ae12
neerajrpatil <118601363+neerajrpatil@users.noreply.github.com>
2024-07-10 15:37:49 +0530

    Time: 22 ms (83.21%), Space: 20 MB (82.3%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.cpp
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -10,30 +10,21 @@ public:
 
 class Solution {
 public:
-    map<int,int>imp;
-    void dfs(int u,int &res, map<int,int>& vis, map<int,vector<int>>& adj){
-        vis[u]=1;
-        res+=imp[u];
-
-        for(int v:adj[u]){
-            if(!vis[v]){
-                dfs(v,res,vis,adj);
-            }
-        }
-    }
     int getImportance(vector<Employee*> employees, int id) {
-        
-        map<int,vector<int>>adj;
+        map<int,Employee*>mp;
         for(auto it:employees){
-            int uid=it->id;
-            imp[uid]=it->importance;
-            for(int v:it->subordinates){
-                adj[uid].push_back(v);
+            mp[it->id]=it;
+        }
+        queue<int>q;
+        int res=0;
+        q.push(id);
+        while(!q.empty()){
+            int uId=q.front();q.pop();
+            res+=mp[uId]->importance;
+            for(int vId:mp[uId]->subordinates){
+                q.push(vId);
             }
         }
-        int res=0;
-        map<int,int>vis;
-        dfs(id,res,vis,adj);
         return res;
     }
 };
